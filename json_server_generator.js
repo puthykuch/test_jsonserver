c6e13a916aa0c3796967992d370b92861eb9709b
puthykuch <puthykuch@gmail.com>
2019-06-21 21:38:38 +0700

    add json server db generator

---
+++ json_server_generator.js
@@ -0,0 +1,24 @@
+module.exports = function() {
+  var faker = require('faker');
+  var _ = require('lodash');
+
+  return {
+    users: _.times(10, function(n) {
+      return {
+        id: n,
+        firstName: faker.name.findName(),
+        lastName: faker.name.lastName(),
+        avatar: faker.internet.avatar(),
+        age: faker.random.number(18, 60),
+        companyId: faker.random.number(5)
+      };
+    }),
+    companies: _.times(5, function(n) {
+      return {
+        id: n,
+        name: faker.company.companyName(),
+        desciptions: faker.company.catchPhraseDescriptor()
+      };
+    })
+  };
+};
